028eb53f6bfe025bca7dae492cb3462b0ce3f6ab
Evan Gora <evan.gora1@gmail.com>
2024-05-31 22:52:17 -0400

    Removed repeating ids from matches and season stats

--- importdata.py
+++ importdata.py
@@ -1,75 +1,44 @@
-# File that imports all data from the CSV files into the models
-
-import csv
+# File that cleans the data to remove the numbered column from the csv file
 from datetime import datetime
-from home.models import *
+import pandas as pd
 
 # Import season stats
-def importStats(filePath):
-    with open(filePath, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            seasonStats.objects.create(
-                season = row['Season'],
-                team = row['Squad'],
-                wins = row['W'],
-                draws = row['D'],
-                losses = row['L'],
-                goalsF = row['GF'],
-                goalsA = row['GA'],
-                points = row['Pts'],
-                shots = row['Sh'],
-                shotsOT = row['SoT'],
-                freeK = row['FK'],
-                PKGoals = row['PK'],
-                passCmp = row['Cmp'],
-                passAtt = row['Att'],
-                passPerc = row['Cmp%'],
-                corners = row['CK'],
-                yellows = row['CrdY'],
-                reds = row['CrdR'],
-                fouls = row['Fls'],
-                PKCon = row['PKcon'],
-                ownGoals = row['OG']
-            )
+def cleanStats():
+    stats = pd.read_csv('data/seasonstats.csv')
+    stats = stats.drop(columns="Unnamed: 0", axis = 1, inplace = False)
+    return stats
 
 # Import matches
-def importMatches(filePath):
-    with open(filePath, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            matches.objects.create(
-                season = row['Season'],
-                date = row['Date'],
-                home = row['Home'],
-                homexG = row['xG'],
-                homeGoals = row['Home Goals'],
-                awayGoals = row['Away Goals'],
-                awayxG = row['xG.1'],
-                away = row['Away'],
-                attendance = row['Attendance'],
-                venue = row['Venue']
-            )
+def cleanMatches():
+    matches = pd.read_csv('data/matches.csv')
+    matches = matches.drop(columns="Unnamed: 0", axis = 1, inplace = False)
+    return matches
 
 # Import seasons
-def importSeasons(filePath):
-    with open(filePath, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            seasons.objects.create(
-                season = row['0']
-            )
-            
-def importTeams(filePath):
-    with open(filePath, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            teams.objects.create(
-                team = row['0']
-            )
+def cleanSeasons():
+    seasons = pd.read_csv('data/seasons.csv')
+    seasons = seasons.drop(columns="Unnamed: 0", axis = 1, inplace = False)
+    return seasons
+                
+def cleanTeams():
+    teams = pd.read_csv('data/teams.csv')
+    teams = teams.drop(columns="Unnamed: 0", axis = 1, inplace = False)
+    return teams
+    
+    
             
 if __name__ == '__main__':
-    importTeams('data/teams.csv')
-    importSeasons('data/seasons.csv')
-    importMatches('data/matches.csv')
-    importStats('data/seasonstats.csv')
+    teams = cleanTeams()
+    seasons = cleanSeasons()
+    matches = cleanMatches()
+    stats = cleanStats()
+    
+    print(teams.info())
+    print(seasons.info())
+    print(matches.info())
+    print(stats.info())
+    
+    teams.to_csv('data/teams.csv')
+    seasons.to_csv('data/seasons.csv')
+    matches.to_csv('data/matches.csv')
+    stats.to_csv('data/seasonstats.csv')
