e745efc071f91e73cc9e03c4e3673522be66f309
Evan Gora <evan.gora1@gmail.com>
2024-06-09 13:53:44 -0400

    Reset the ids for the data

--- cleandata.py
+++ cleandata.py
@@ -5,7 +5,8 @@ import pandas as pd
 # Import season stats
 def cleanStats():
     stats = pd.read_csv('data/seasonstats.csv')
-    stats = stats.drop(columns="Unnamed: 0", axis = 1, inplace = False)
+    stats = stats.drop(columns=['Unnamed: 0.2'], axis = 1, inplace=False)
+    stats = stats.drop(stats.columns[0], axis=1, inplace=False)
     return stats
 
 # Import matches
@@ -16,25 +17,27 @@ def cleanMatches():
     for i in range(0, len(vals)):
         if vals[i]:
             matches = matches.drop(i)
-    matches = matches.drop(columns="Unnamed: 0", axis = 1, inplace = False)
+    matches = matches.drop(columns=['Unnamed: 0.2'], axis = 1, inplace=False)
+    matches = matches.drop(matches.columns[0], axis=1, inplace=False)
     return matches
                 
 def cleanTeams():
     teams = pd.read_csv('data/teams.csv')
-    teams = teams.drop(columns="Unnamed: 0", axis = 1, inplace = False)
+    teams = teams.sort_values('0')
+    teams = teams.drop(teams.columns[0], axis = 1, inplace = False)
     return teams
     
     
             
 if __name__ == '__main__':
-   # teams = cleanTeams()
+    teams = cleanTeams()
     matches = cleanMatches()
-    #stats = cleanStats()
+    stats = cleanStats()
     
-   # print(teams.info())
-    print(matches.info())
-    #print(stats.info())
+    print(teams)
+    print(matches)
+    print(stats)
     
-   # teams.to_csv('data/teams.csv')
+    teams.to_csv('data/teams.csv')
     matches.to_csv('data/matches.csv')
-    #stats.to_csv('data/seasonstats.csv')
+    stats.to_csv('data/seasonstats.csv')
