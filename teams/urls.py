f47d98e27c1b8b0227c9d5887f8f797f1c45f8f1
Evan Gora <evan.gora1@gmail.com>
2024-06-09 21:24:31 -0400

    Work on loading data by team

--- teams/urls.py
+++ teams/urls.py
@@ -3,5 +3,5 @@ from teams import views
     
 urlpatterns = [
     path("", views.teams, name="teams"),
-    #path("<str:usrteam>/", views.teamview, name="teamview")
+    path("<str:usrteam>/", views.teamview, name="teamview")
 ]
--- teams/views.py
+++ teams/views.py
@@ -6,3 +6,9 @@ from home.models import *
 def teams(request):
     teams = teamsTbl.objects.all()
     return render(request, 'teams/teamshome.html', {"teams":teams})
+
+def teamview(request, usrteam):
+    stats = statsTbl.objects.all().filter(squad=usrteam)
+    matches = matchesTbl.objects.all().filter(home=usrteam).filter(away=usrteam)
+    context = {"stats": stats, "matches": matches}
+    return render(request, 'teams/teampage.html', context)
