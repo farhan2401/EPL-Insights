7a77fe235c2cb5be836331460eb10d3c3f919e97
Evan Gora <evan.gora1@gmail.com>
2024-06-12 22:11:12 -0400

    Display matches for each team by season

--- teams/views.py
+++ teams/views.py
@@ -13,7 +13,14 @@ def teamview(request, usrteam):
     return render(request, 'teams/teampage.html', context)
 
 def teammatchview(request, usrteam, usrseason):
-    home = matchesTbl.objects.all().filter(home=usrteam).filter(season=usrseason)
-    away = matchesTbl.objects.all().filter(away=usrteam).filter(season=usrseason)
-    context = {"home": home, "away": away}
-    return HttpResponse("Matches")
+    homeGames = matchesTbl.objects.all().filter(home=usrteam).filter(season=usrseason)
+    awayGames = matchesTbl.objects.all().filter(away=usrteam).filter(season=usrseason)
+    context = {"homeGames": homeGames, "awayGames": awayGames}
+    if usrseason == "2017-2018":
+        return render(request, 'teams/20172018page.html', context)
+    elif int(usrseason[:4]) > 2017:
+        return render(request, 'teams/nomissingdata.html', context)
+    elif int(usrseason[:4]) < 2017 and int(usrseason[:4]) > 1991:
+        return render(request, 'teams/2016-1992.html', context)
+    else:
+        return render(request, 'teams/1991end.html', context)
