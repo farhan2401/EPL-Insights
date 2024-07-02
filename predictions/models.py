885ffbef5b673555044177fad09a738f2fb2f358
Evan Gora <evan.gora1@gmail.com>
2024-07-02 13:18:18 -0400

    Added CSV for the forst 10 weeks of upcoming matches

--- predictions/models.py
+++ predictions/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class upcomingMatches(models.Model):
     date = models.CharField(max_length=15, db_column="date")
+    week = models.IntegerField()
     home = models.CharField(max_length=20, db_column="home")
     away =  models.CharField(max_length=20, db_column="away")
     venue = models.CharField(max_length=40, db_column="venue")
